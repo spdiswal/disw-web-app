09f972a05ac46d2b5ddf7d8b4d19a2cf273e976b
Steffen Diswal <11992328+spdiswal@users.noreply.github.com>
2022-02-05 00:54:05 +0100

    Revert to complete client-side rendering
    Currently, the server-oriented build produces an `index.html` page in
    Danish with the light theme. However, these assumptions are broken when
    the browser prefers the English locale or the dark theme.
    
    Relying on these assumptions, the `hydrate` call in `main-client.tsx`
    ends up rendering an incorrect DOM tree, most notably observed in the
    initial appearance of the theme picker and the locale picker.
    
    This commit reverts to calling `render` in `main-client.tsx`. It may
    cause the browser to render the page in Danish for a brief moment before
    Preact re-renders it in English.
    
    Pre-rendering `index.html` is still desired to allow search engines to
    discover the contents of the app, albeit only in Danish for now.

--- main/main-client.tsx
+++ main/main-client.tsx
@@ -1,10 +1,10 @@
 import { adaptLocaleCachePortToSessionStorage, adaptPreferredLocalePortToNavigatorLanguages } from "+i18n"
 import { adaptMediaThemePortToMediaQuery, adaptThemeCachePortToSessionStorage } from "+theme"
-import { hydrate } from "preact"
+import { render } from "preact"
 import { App } from "./App"
 import "./index.css"
 
-hydrate(
+render(
     <App
         localeCachePort={adaptLocaleCachePortToSessionStorage()}
         mediaThemePort={adaptMediaThemePortToMediaQuery()}
